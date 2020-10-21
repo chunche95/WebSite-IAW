296c6869c55bd042f6c62557adeb6fb120f1a7e4
chunche95 <paeste95.pb@gmail.com>
2020-10-21 21:14:18 +0200

    Finish page of the quiz project

---
+++ BuildAQuizApp-master/end.js
@@ -0,0 +1,29 @@
+/* Constants */
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+/* Your note */
+finalScore.innerText = mostRecentScore;
+/* Register username */
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+/* Scores */
+saveHighScore = e => {
+  console.log("clicked the save button!");
+  e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("index.html");
+};
